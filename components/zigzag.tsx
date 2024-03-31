923ae83671441c27ed40c3ccb9caac7d9a18e366
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-03-31 20:41:58 -0300

    chore: altera textos valor

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -48,7 +48,7 @@ export default function Zigzag() {
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Adulto: 130 Reais, Criança: 150 Reais</span>
+                      <span>Adulto: R$ 130, Criança: R$ 150</span>
                     </li>
                   </ul>
                 </div>
@@ -77,13 +77,13 @@ export default function Zigzag() {
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>2x na semana: 100 Reais</span>
+                      <span>2x na semana: R$ 100</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>3x na semana: 130 Reais</span>
+                      <span>3x na semana: R$ 130</span>
                     </li>
                   </ul>
                 </div>
@@ -112,7 +112,7 @@ export default function Zigzag() {
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>100 Reais</span>
+                      <span>R$ 100</span>
                     </li>
                   </ul>
                 </div>
