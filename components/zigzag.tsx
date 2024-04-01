218eda498d93de4426a34bf3f7c662eb12668fa9
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-04-01 11:32:17 -0300

    chore: add mb-2

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -44,13 +44,13 @@ export default function Zigzag() {
                       </svg>
                       <span>Criança - Terça e Quinta: 18h às 19h</span>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
                       <span>Adulto: R$ 130, Criança: R$ 150</span>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
@@ -85,13 +85,13 @@ export default function Zigzag() {
                       </svg>
                       <span>2x na semana: R$ 100</span>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
                       <span>3x na semana: R$ 130</span>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-red-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
