0a76da2e0af139d4a382af26749e5011d38a7b47
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-03-31 21:01:50 -0300

    chore: aumento de icones e reposicionamento

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             </li> */}
             <li className="ml-4">
               <Link href="https://www.instagram.com/shizentai_oficial/" target="_blank" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Instagram">
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-12 h-12 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                   <circle cx="20.145" cy="11.892" r="1" />
                   <path d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z" />
                   <path d="M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z" />
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -33,25 +33,32 @@ export default function Header() {
           </div>
 
           
-          {/* <nav className="hidden md:flex md:grow">
+          <nav className="hidden md:flex md:grow">
             
             <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+              <Link href="https://www.instagram.com/shizentai_oficial/" target="_blank" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Instagram">
+                <svg className="w-12 h-12 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="20.145" cy="11.892" r="1" />
+                  <path d="M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z" />
+                  <path d="M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z" />
+                </svg>
+              </Link>
+              {/* <li>
+                <Link href="/signin" className="font-medium text-gray-400 hover:text-gray-100 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
               </li>
               <li>
-                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link href="/signup" className="btn-sm text-gray-800 bg-gray-100 hover:bg-gray-200 ml-3">
                   <span>Sign up</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
                 </Link>
-              </li>
+              </li> */}
             </ul>
 
           </nav>
 
-          <MobileMenu /> */}
+          {/* <MobileMenu /> */}
 
         </div>
       </div>
