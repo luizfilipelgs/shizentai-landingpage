3a2701242b84c32a818546e5589e6c68f797deda
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-04-04 15:13:03 -0300

    fix: add btn fechar video

--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -75,6 +75,12 @@ export default function ModalVideo({
           >
             <div className="max-w-6xl mx-auto h-full flex items-center justify-center">
               <Dialog.Panel className="max-w-full h-full bg-black overflow-hidden flex justify-center items-center">
+              <button
+                className="absolute top-0 right-0 m-4 z-50 p-2 text-white bg-red-500 rounded-full"
+                onClick={() => setModalOpen(false)}
+              >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+              </button>
               <iframe
                 src={video}
                 width={videoWidth}
