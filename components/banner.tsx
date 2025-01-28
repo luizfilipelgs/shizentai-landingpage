fa647de5dd831cd0564c1d1895f19ef8422206ed
luiz filipe de lima <luizfilipelgs@gmail.com>
2025-01-28 12:48:14 -0300

    feat: return whatsapp contact

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,18 +4,21 @@ import { schedulingLink } from '@/utils/links'
 import Link from 'next/link'
 import { useState } from 'react'
 import { FaCalendarAlt } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Banner() {
 
   const [bannerOpen, setBannerOpen] = useState<boolean>(true)
+  const whatsLink = 'https://wa.link/fzwxqj';
+
   return (
     <>
       {bannerOpen && (
         <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
           <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
             <div className='text-slate-500 inline-flex'>
-              <Link href={schedulingLink} target="_blank" className="flex justify-center items-center text-white bg-black-500 hover:bg-red-600 rounded-full transition duration-150 ease-in-out" aria-label="WhatsApp">
-                <FaCalendarAlt className="w-8 h-8 fill-current" />
+              <Link href={whatsLink} target="_blank" className="flex justify-center items-center text-white bg-black-500 hover:bg-red-600 rounded-full transition duration-150 ease-in-out" aria-label="WhatsApp">
+                <FaWhatsapp className="w-8 h-8 fill-current" />
               </Link>
             </div>
             <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>
